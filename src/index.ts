0de1dc01e320637986c58d43c402cce74ae59f60
Kim Ode <kim.gronqvist@conrab.se>
2022-02-25 14:59:38 +0100

    Add intersection, validation rules

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,144 @@
-import { getType } from 'mobx-state-tree';
+import { getType, IAnyType, Instance, isType, types } from 'mobx-state-tree';
 
-function setError(paths: any, message: any, validations: any) {
-    if (paths.length && !paths[0].path) {
-        setError(paths.slice(1), message, validations);
+// union - No type is applicable for the union
+// identifier - Value is not a valid identifier, expected a string
+// identifierNumber - Value is not a valid identifierNumber, expected a number
+
+// undefined - Value is not a undefined
+// null - Value is not a null
+// string - Value is not a string
+// number - Value is not a number
+// integer - Value is not a integer
+// Date - Value is not a Date or unix milliseconds timestamp
+// array - Value is not an array
+// boolean - Value is not a boolean
+// enumeration - Value is not a literal
+// literal - Value is not a literal
+// map - Value is not a plain object
+
+const VALIDATION_IDENTIFIER = '\0Validation';
+const INTERESCTION_IDENTIFIER = '\0IntersectionValidation';
+const INTERSECTION_SEPERATOR = ' & ';
+
+export function getErrorMessages(errorMessage: string): string | string[] {
+    const parse = (error: string) => {
+        const match = error.match(/Invalid value for type (.*)/);
+        if (match) {
+            return match[1].split(':')[1].trim();
+        }
+        return '';
+    };
+
+    const intersection = errorMessage.includes(INTERESCTION_IDENTIFIER);
+    if (intersection) {
+        return parse(errorMessage).split(INTERSECTION_SEPERATOR);
+    }
+
+    const validation = errorMessage.includes(VALIDATION_IDENTIFIER);
+    if (validation) {
+        return parse(errorMessage);
+    }
+
+    return errorMessage;
+}
+
+function getErrors(errors: undefined | any[], errorMessage: string) {
+    return [...(errors ?? []), getErrorMessages(errorMessage)].flat().filter(Boolean);
+}
+
+function setError(paths: any, message: any, validationLevel: any, allErrors: any[]) {
+    const path = paths[0].path;
+    if (!path && paths.length === 1) {
+        const errors = getErrors(validationLevel.errors, message);
+        validationLevel.isValid = false;
+        validationLevel.errors = errors;
+        allErrors.push(...errors);
+    } else if (!path && paths.length > 1) {
+        setError(paths.slice(1), message, validationLevel, allErrors);
     } else if (paths.length > 1) {
-        validations[paths[0].path] = validations[paths[0].path] ? validations[paths[0].path] : {};
-        setError(paths.slice(1), message, validations[paths[0].path]);
+        validationLevel[path] = validationLevel[path] ?? {};
+        setError(paths.slice(1), message, validationLevel[path], allErrors);
     } else {
-        if (validations[paths[0].path] && validations[paths[0].path].messages) {
-            validations[paths[0].path].messages.push(message);
-            validations[paths[0].path].isValid = false;
-        } else {
-            validations[paths[0].path] = {
-                messages: [message],
-                isValid: false,
-            };
-        }
+        const errors = getErrors(validationLevel.errors, message);
+        validationLevel[path] = { isValid: false, errors };
+        allErrors.push(...errors);
     }
 }
 
-function setValidationResult(data: any, validations: any) {
-    for (let [key, value] of Object.entries(data)) {
-        if (isObject(value)) {
-            setValidationResult(data[key], validations[key]);
-        } else {
-            validations[key] =
-                validations[key] !== undefined ? validations[key] : { isValid: true, messages: [] };
-        }
+function setValidations(data: any, validations: any) {
+    if (!isObject(data)) {
+        return validations?.errors
+            ? { ...validations, value: data }
+            : { isValid: true, errors: [], value: data };
     }
+
+    for (let [key] of Object.entries(data)) {
+        validations[key] = setValidations(data[key], validations[key]);
+    }
+    return validations;
 }
 
-// TODO: Add overload for using either model or type
-export function validate(model: any, data: any): any {
-    const type = getType(model);
+export function validation<T>(validator: IAnyType, error: ((value: T) => string) | string) {
+    return types.custom({
+        name: VALIDATION_IDENTIFIER,
+        fromSnapshot(value: T) {
+            return value;
+        },
+        toSnapshot(value: T) {
+            return value;
+        },
+        isTargetType(value: T) {
+            return false; // always validate
+        },
+        getValidationMessage(value: T): string {
+            const { isValid } = validate(validator, value);
+            return isValid ? '' : typeof error === 'string' ? error : `${error(value)}`;
+        },
+    });
+}
+
+export function intersection<T>(...validators: IAnyType[]) {
+    return types.custom({
+        name: INTERESCTION_IDENTIFIER,
+        fromSnapshot(value: T) {
+            return value;
+        },
+        toSnapshot(value: T) {
+            return value;
+        },
+        isTargetType(value: T) {
+            return false; // always validate
+        },
+        getValidationMessage(value: T): string {
+            const invalid = validators.flatMap((v) => validate(v, value).errors);
+            return !invalid.length ? '' : `${invalid.join(INTERSECTION_SEPERATOR)}`;
+        },
+    });
+}
+
+export function validate<T extends IAnyType>(modelOrType: T | Instance<T>, data: any): any {
+    const type = isType(modelOrType) ? modelOrType : getType(modelOrType);
     const mstValidations = type.validate(data, [{ path: '', type }]);
 
-    let validations = {};
-    for (let validation of mstValidations) {
-        setError(validation.context, validation.message, validations);
-    }
+    let validationErrors = {};
+    let errors: any[] = [];
+    mstValidations.forEach((v) => {
+        setError(v.context, v.message, validationErrors, errors);
+    });
 
-    setValidationResult(data, validations);
+    const validations = setValidations(data, validationErrors);
 
-
-    
     return {
         isValid: mstValidations.length ? false : true,
         validations,
+        errors,
     };
 }
+
+export const rules = {
+    validation,
+    intersection,
+};
 
 function isObject(o: any) {
     return Object.prototype.toString.call(o) === '[object Object]';
--- tests/mstValidation.test.ts
+++ tests/mstValidation.test.ts
@@ -1,18 +1,50 @@
-import { types } from 'mobx-state-tree';
-import { validate } from '../src/';
+import { applySnapshot, getSnapshot, SnapshotIn, types } from 'mobx-state-tree';
+import { validate, rules } from '../src/';
+
+const min = (min: number) =>
+    types.refinement(
+        types.number,
+        (v) => v >= min,
+        () => 'min'
+    );
+
+const minLength = (min: number) =>
+    types.refinement(
+        types.string,
+        (v) => v.length >= min,
+        () => 'minLength'
+    );
+const maxLength = (max: number) =>
+    types.refinement(
+        types.string,
+        (v) => v.length <= max,
+        () => 'maxLength'
+    );
 
 test('basic validation', () => {
-    const Model = types.model({
-        age: types.refinement(types.number, (v) => v >= 0, () => 'Not a valid age'),
-        pets: types.number,
-        name: types.string,
-    });
+    const Model = types
+        .model({
+            age: types.refinement(
+                types.number,
+                (v) => v > 0,
+                () => 'Is not a valid age'
+            ),
+            pets: types.number,
+            name: types.string,
+        })
+        .actions((self) => ({
+            setAge(age: number) {
+                self.age = age;
+            },
+        }));
+
     const m = Model.create({ age: 4, name: 'test', pets: 2 });
     const { validations, isValid } = validate(m, { age: -2, name: 'kim', pets: 'dog' });
+
     expect(isValid).toBe(false);
     expect(validations.age.isValid).toBe(false);
-    expect(validations.age.messages[0]).toBe('Not a valid age');
-    expect(validations.pets.messages[0]).toBe('Value is not a number');
+    expect(validations.age.errors[0]).toBe('Is not a valid age');
+    expect(validations.pets.errors[0]).toBe('Value is not a number');
 });
 
 test('nested model', () => {
@@ -21,16 +53,105 @@ test('nested model', () => {
             l2: types.model({
                 l3: types.model({
                     l4: types.model({
-                        name: types.string,
+                        name: rules.validation(types.string, 'name'),
                     }),
                 }),
             }),
         }),
         age: types.maybe(types.number),
     });
-    const m = Model.create({ l1: { l2: { l3: { l4: { name: 'test' }} } } });
-    const { validations } = validate(m, { age: 4, l1: { l2: { l3: { l4: { name: 4 }} } } });
+
+    const m = Model.create({ l1: { l2: { l3: { l4: { name: 'test' } } } } });
+    const { validations } = validate(m, { age: 4, l1: { l2: { l3: { l4: { name: 4 } } } } });
     expect(validations.l1.l2.l3.l4.name.isValid).toBe(false);
-    expect(validations.l1.l2.l3.l4.name.messages[0]).toBe('Value is not a string');
+    expect(validations.l1.l2.l3.l4.name.errors[0]).toBe('name');
     expect(validations.age.isValid).toBe(true);
+});
+
+test('primitive type', () => {
+    const t = types.union(types.string, types.undefined);
+    const union = validate(t, null);
+    expect(union.isValid).toBe(false);
+    expect(union.validations.errors).toEqual([
+        'No type is applicable for the union',
+        'Value is not a string',
+        'Value is not a undefined',
+    ]);
+
+    const i = rules.intersection<string>(minLength(1), maxLength(5));
+    const intersection = validate(i, '2222222222');
+    expect(intersection.errors[0]).toBe('maxLength');
+});
+
+test('basic form', () => {
+    const UserModel = types.model({
+        age: rules.validation(rules.intersection(types.integer, min(0)), 'Not a valid age'),
+    });
+
+    const Field = types.model({
+        errors: types.array(types.string),
+        isValid: true,
+    });
+    const Input = Field.named('Input')
+        .props({
+            value: '',
+        })
+        .actions((self) => ({
+            setValue(value: string) {
+                self.value = value;
+            },
+        }));
+    const IntegerInput = Input.named('IntegerInput')
+        .preProcessSnapshot((snapshot: SnapshotIn<typeof Input>) => {
+            return {
+                value: `${snapshot.value}`,
+                isValid: snapshot.isValid,
+                errors: snapshot.errors,
+            };
+        })
+        .postProcessSnapshot((snapshot) => {
+            const parsed = parseInt(snapshot.value, 0);
+            return Number.isInteger(parsed) ? parsed : snapshot.value;
+        });
+    const FormModel = types.model('FormModel', {}).views((self) => ({
+        get isValid() {
+            return Object.values(self).every((v) => v.isValid);
+        },
+    })).actions(self => ({
+        init(data: any) {
+            let snapshot: any = {};
+            for (let [key, value] of Object.entries(data)) {
+                snapshot[key] = { value, isValid: true, errors: [] };
+            }    
+            applySnapshot(self, snapshot);
+        }
+    }))
+
+    const UserFormModel = FormModel.named('UserFormModel')
+        .props({
+            age: types.optional(IntegerInput, {}),
+        })
+        .actions((self) => ({
+            validate() {
+                const { validations } = validate(UserModel, getSnapshot(self));
+                applySnapshot(self, validations);
+            },
+        }));
+
+    const user = UserModel.create({ age: 4 });
+    const form = UserFormModel.create();
+    form.init(user);
+    form.validate();
+
+    form.age.setValue('a');
+    form.validate();
+
+    expect(form.isValid).toBe(false);
+
+    form.age.setValue('9');
+    form.validate();
+
+    expect(form.isValid).toBe(true);
+    applySnapshot(user, getSnapshot(form));
+    expect(user.age).toBe(9);
 });
